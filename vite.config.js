f02947abb701389aaa1a03ced76895697b7cff45
Luis Andres lu. Lopez Frias <lalopezf@LICONSA.CENTRAL>
2025-10-22 10:05:26 -0600

    INSTALACION DE FILAMENT V4 Y AJUSTES DE LA RUTA LANDING-PAGE

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
-// C:\laragon\www\landing_page\vite.config.js
-
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite'; // ⬅️ DEBE ESTAR IMPORTADO
 
-// 1. Importar el plugin de Tailwind v4
+import laravel from 'laravel-vite-plugin';
+// ⚠️ MANTENER SOLO UNA VEZ ESTA LÍNEA 
 import tailwindcss from '@tailwindcss/vite'; 
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js',
+                // Filament requiere que sus propios assets se incluyan en el input
+                'resources/css/filament/admin/theme.css', 
+            ],
             refresh: true,
         }),
-        // 2. Añadir el plugin de Tailwind
+        // ⚠️ MANTENER SOLO UNA VEZ ESTA LÍNEA
         tailwindcss(), 
     ],
 });
+
 
